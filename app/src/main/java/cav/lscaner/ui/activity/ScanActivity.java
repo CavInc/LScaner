53a477afc0ac8131e9e4272a4e6e801704f89e74
CavInc <cavinc20@gmail.com>
2017-10-27 23:41:22 +0700

    Работа над разным

--- app/src/main/java/cav/lscaner/ui/activity/ScanActivity.java
+++ app/src/main/java/cav/lscaner/ui/activity/ScanActivity.java
@@ -1,10 +1,17 @@
 package cav.lscaner.ui.activity;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -38,7 +45,27 @@ public class ScanActivity extends AppCompatActivity {
 
         mListView = (ListView) findViewById(R.id.san_lv);
 
+        mBarCode.addTextChangedListener(mBarWatcher);
+        mBarCode.setOnEditorActionListener(mEditorActionListener);
+
+        setupToolBar();
         updateUI();
+
+    }
+
+    public void setupToolBar(){
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return true;
     }
 
     private void updateUI(){
@@ -52,4 +79,29 @@ public class ScanActivity extends AppCompatActivity {
         }
 
     }
+
+    TextWatcher mBarWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            Log.d("SA","END CHANGE");
+        }
+    };
+
+    TextView.OnEditorActionListener mEditorActionListener = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+            Log.d("SA",textView.getText().toString());
+            return false;
+        }
+    };
 }
--- app/src/main/java/cav/lscaner/ui/dialogs/QueryQuantityDialog.java
+++ app/src/main/java/cav/lscaner/ui/dialogs/QueryQuantityDialog.java
@@ -1,8 +1,46 @@
 package cav.lscaner.ui.dialogs;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import cav.lscaner.R;
 
 public class QueryQuantityDialog extends DialogFragment{
 
+    private TextView mName;
+    private EditText mQuantity;
 
+    private QuantityChangeListener mQuantityChangeListener;
+
+    public interface QuantityChangeListener {
+        public void changeQuantity (Float quantity);
+    }
+
+    public static QueryQuantityDialog newInstans(){
+        Bundle args = new Bundle();
+
+        QueryQuantityDialog dialog = new QueryQuantityDialog();
+        return dialog;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.queryquantity_dialog, null);
+
+        mName = (TextView) v.findViewById(R.id.qq_title);
+        mQuantity = (EditText) v.findViewById(R.id.qq_quantity);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Количество товара").setView(v);
+
+        return builder.create();
+    }
 }
