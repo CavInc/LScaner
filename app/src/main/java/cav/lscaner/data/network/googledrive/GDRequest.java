a4863f180aa880575cdbb267647b0269b49084fd
CavInc <cavinc20@gmail.com>
2017-10-31 16:49:13 +0700

    Рыба для гуглодрайва

---
+++ app/src/main/java/cav/lscaner/data/network/googledrive/GDRequest.java
@@ -0,0 +1,8 @@
+package cav.lscaner.data.network.googledrive;
+
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+
+public class GDRequest {
+    GoogleAccountCredential mCredential;
+
+}
