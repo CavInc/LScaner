240b489014036cdef6c399af022db4de87b8e5c5
CavInc <cavinc20@gmail.com>
2017-11-15 11:02:49 +0700

    Работа над настройками полей файла для чтения

---
+++ app/src/main/java/cav/lscaner/ui/activity/SettingFieldFileActivity.java
@@ -0,0 +1,15 @@
+package cav.lscaner.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cav.lscaner.R;
+
+public class SettingFieldFileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_field_file);
+    }
+}
