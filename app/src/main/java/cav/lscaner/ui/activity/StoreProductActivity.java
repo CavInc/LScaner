854824d70d4c4feb769e22270912c4bd92e3496a
CavInc <cavinc20@gmail.com>
2017-12-07 13:02:15 +0700

    Добавлен поиск в список товаров

--- app/src/main/java/cav/lscaner/ui/activity/StoreProductActivity.java
+++ app/src/main/java/cav/lscaner/ui/activity/StoreProductActivity.java
@@ -1,8 +1,14 @@
 package cav.lscaner.ui.activity;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
@@ -18,6 +24,9 @@ public class StoreProductActivity extends AppCompatActivity {
     private DataManager mDataManager;
     private ListView mListView;
 
+    private StoreProductAdapter adapter;
+    private MenuItem searchItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +36,7 @@ public class StoreProductActivity extends AppCompatActivity {
         mListView = (ListView) findViewById(R.id.store_p_lv);
 
         ArrayList<StoreProductModel> models = mDataManager.getStoreProdect();
-        StoreProductAdapter adapter = new StoreProductAdapter(this,R.layout.store_product_item,models);
+        adapter = new StoreProductAdapter(this,R.layout.store_product_item,models);
         mListView.setAdapter(adapter);
 
         setupToolBar();
@@ -38,6 +47,50 @@ public class StoreProductActivity extends AppCompatActivity {
         if (actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.scan_menu, menu);
+        searchItem = menu.findItem(R.id.scan_menu_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        if(null!=searchManager ) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        } else {
+            return true;
+        }
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return  false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                int pos = adapter.getPosition(new StoreProductModel("",newText));
+                //Log.d("SP","POS : "+pos);
+                mListView.setSelection(pos);
+                return  true;
+            }
+        });
+
+        MenuItemCompat.setOnActionExpandListener(searchItem,new MenuItemCompat.OnActionExpandListener(){
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                //mAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+
+
+        return true;
     }
 
     @Override
