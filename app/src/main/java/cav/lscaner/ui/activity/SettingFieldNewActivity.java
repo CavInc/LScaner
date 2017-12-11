7f1a1225b1e5c3fc28557871382f3c99ebeed93d
CavInc <cavinc20@gmail.com>
2017-12-11 23:43:24 +0700

    Работа над настройками полей

--- app/src/main/java/cav/lscaner/ui/activity/SettingFieldNewActivity.java
+++ app/src/main/java/cav/lscaner/ui/activity/SettingFieldNewActivity.java
@@ -3,8 +3,10 @@ package cav.lscaner.ui.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import cav.lscaner.R;
@@ -20,15 +22,115 @@ public class SettingFieldNewActivity extends AppCompatActivity {
     // названия  (групп)
     String[] groups = new String[] {"База данных", "Товар", "ЕГАИС","Переоценка","Поступление"};
 
+    String[] storeProduct = new String[] {"Штрих-код","Код","Наименование",
+            "Остаток","Цена","Цена закупочная","Код ЕГАИС","Артикул"};
+    String[] tovar = new String[]{"Штрих-код","Код","Кол-во."};
+    String[] egais = new String[] {"Штрих-код","Код","Кол-во.","Код ЕГАИС"};
+    String[] changePrice = new String[]{"Штрих-код","Код","Цена"};
+    String[] prihod = new String[] {"Штрих-код","Код","Кол-во.","Цена закупочная"};
+
+    // коллекция для групп
+    ArrayList<Map<String, String>> groupData;
+
+    // коллекция для элементов одной группы
+    ArrayList<Map<String, String>> childDataItem;
+
+    // общая коллекция для коллекций элементов
+    ArrayList<ArrayList<Map<String, String>>> childData;
+
+    // список атрибутов группы или элемента
+    Map<String, String> m;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_field_new);
 
-        // коллекция для групп
-        ArrayList<Map<String, String>> groupData;
+        // заполняем коллекцию групп из массива с названиями групп
+        groupData = new ArrayList<Map<String, String>>();
+        for (String group : groups) {
+            // заполняем список атрибутов для каждой группы
+            m = new HashMap<String, String>();
+            m.put("groupName", group); // имя раздела
+            groupData.add(m);
+        }
+        // список атрибутов групп для чтения
+        String groupFrom[] = new String[] {"groupName"};
+        // список ID view-элементов, в которые будет помещены атрибуты групп
+        int groupTo[] = new int[] {R.id.elg_name};
+
+
+        // создаем коллекцию для коллекций элементов
+        childData = new ArrayList<ArrayList<Map<String, String>>>();
+
+        // создаем коллекцию элементов для первой группы
+        childDataItem = new ArrayList<Map<String, String>>();
+        // заполняем список атрибутов для каждого элемента
+        for (String storeP : storeProduct) {
+            m = new HashMap<String, String>();
+            m.put("itemText", storeP);
+            childDataItem.add(m);
+        }
+        // добавляем в коллекцию коллекций
+        childData.add(childDataItem);
+
+        // создаем коллекцию элементов для второй группы
+        childDataItem = new ArrayList<Map<String, String>>();
+        // заполняем список атрибутов для каждого элемента
+        for (String item : tovar) {
+            m = new HashMap<String, String>();
+            m.put("itemText", item);
+            childDataItem.add(m);
+        }
+        childData.add(childDataItem);
+
+
+        // создаем коллекцию элементов для третьей группы
+        childDataItem = new ArrayList<Map<String, String>>();
+        for (String item : egais) {
+            m = new HashMap<String, String>();
+            m.put("itemText", item);
+            childDataItem.add(m);
+        }
+        childData.add(childDataItem);
+
+        // создаем коллекцию элементов для четвертой группы
+        childDataItem = new ArrayList<Map<String, String>>();
+        for (String item : changePrice) {
+            m = new HashMap<String, String>();
+            m.put("itemText", item);
+            childDataItem.add(m);
+        }
+        childData.add(childDataItem);
+
+        // создаем коллекцию элементов для пятой группы
+        childDataItem = new ArrayList<Map<String, String>>();
+        for (String item : prihod) {
+            m = new HashMap<String, String>();
+            m.put("itemText", item);
+            childDataItem.add(m);
+        }
+        childData.add(childDataItem);
+
+        // список атрибутов элементов для чтения
+        String childFrom[] = new String[] {"itemText"};
+        // список ID view-элементов, в которые будет помещены атрибуты элементов
+        int childTo[] = new int[] {R.id.expant_list_item_name};
+
+
+        SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(
+                this,
+                groupData,
+                R.layout.expant_list_group_item,
+                groupFrom,
+                groupTo,
+                childData,
+                R.layout.expand_list_item,
+                childFrom,
+                childTo);
 
         mExpandList = (ExpandableListView) findViewById(R.id.expandableListView);
+        mExpandList.setAdapter(adapter);
 
     }
 }
