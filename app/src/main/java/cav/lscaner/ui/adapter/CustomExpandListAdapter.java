82dc79a9f9058e404eb9d43e580dedf1d85c7b7e
CavInc <cavinc20@gmail.com>
2017-12-12 17:03:06 +0700

    Работа над развигающимися элементами

---
+++ app/src/main/java/cav/lscaner/ui/adapter/CustomExpandListAdapter.java
@@ -0,0 +1,69 @@
+package cav.lscaner.ui.adapter;
+
+// Адаптер для раскрывающегося списка
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+import java.util.ArrayList;
+
+public class CustomExpandListAdapter  extends BaseExpandableListAdapter {
+
+    private ArrayList<ArrayList<String>> mGroups;
+    private Context mContext;
+    private int mGroupLayout;
+
+
+
+    @Override
+    public int getGroupCount() {
+
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+
+    }
+
+    @Override
+    public Object getGroup(int i) {
+
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+
+    }
+
+    @Override
+    public long getGroupId(int i) {
+
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+
+    }
+
+    @Override
+    public boolean hasStableIds() {
+
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+
+    }
+}
