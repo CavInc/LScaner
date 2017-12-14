59d34e749b659d3d8fd481a24f78db7d9a3141a4
CavInc <cavinc20@gmail.com>
2017-12-14 16:58:17 +0700

    Добавлены ссылки на ELV

--- app/src/main/java/cav/lscaner/ui/activity/SettingFieldNewActivity.java
+++ app/src/main/java/cav/lscaner/ui/activity/SettingFieldNewActivity.java
@@ -19,6 +19,11 @@ import cav.lscaner.ui.adapter.CustomExpandListAdapter;
 //https://www.programcreek.com/java-api-examples/index.php?api=android.widget.SimpleExpandableListAdapter
 
 //https://sohabr.net/post/227549/   - а вот тут то что надо
+//https://androidexample.com/Custom_Expandable_ListView_Tutorial_-_Android_Example/index.php?view=article_discription&aid=107&aaid=129
+//https://ru.androids.help/q10836    -- только поглядеть возможно и нафиг не нужно
+//https://www.codeproject.com/Articles/1151814/Android-ExpandablelistView-Tutorial-with-Android-C
+//http://abhiandroid.com/ui/expandablelistadapter-example-android.html
+// https://stackoverflow.com/questions/5188196/how-to-write-custom-expandablelistadapter - тоже самое
 
 public class SettingFieldNewActivity extends AppCompatActivity {
 
